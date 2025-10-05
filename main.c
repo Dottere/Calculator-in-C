a3e5e06e1eb66cf3b1ba39a408053f9e5bb2ab2a
Balogh Tamás <balogh.t.1333@gmail.com>
2025-10-05 17:21:59 +0200

    cleaned up main();

--- main.c
+++ main.c
@@ -22,6 +22,14 @@ typedef struct {
 } result;
 
 typedef struct {
+    const wchar_t *welcome;
+    const wchar_t *result;
+    const wchar_t *missingOperand;
+    const wchar_t *zeroDivision;
+    const wchar_t *moduloError;
+} StringArray;
+
+typedef struct {
     wchar_t hu[126];
     wchar_t de[146];
     wchar_t en[135];
@@ -48,6 +56,8 @@ enum errCodes {
 
 calculation evaluateOperation(char *operation);
 result doCalculation(calculation calculation);
+StringArray *setupLanguage(void);
+void checkForErrors(StringArray stringArray);
 
 // Prototypes
 
@@ -56,104 +66,10 @@ int main(void) {
     setlocale(LC_ALL, "");
     setlocale(LC_NUMERIC, "C");
 
-    wprintf(L"Válassz egy nyelvet/Wähle eine Sprache/Choose a language:\n1 Magyar\n2 Detusch\n3 English\n> ");
+    const StringArray *stringArray = setupLanguage();
 
-    char langChar[3];
-    fgets(langChar, 3, stdin);
-    const long lang = strtol(langChar, NULL, 10);
+    checkForErrors(*stringArray);
 
-    const translations translations[] = {
-        {.hu = L"Számológép\n",
-            .de = L"Taschenrechner\n",
-            .en = L"Calculator\n"},
-
-
-        {.hu = L"Ennek a számolásnak az eredménye:",
-            .de = L"Das Ergebnis dieser Berechnung:",
-            .en = L"The result of this calculation:"},
-
-        {.hu = L"Kötelező megadni egy operandust! Ezek lehetnek:\n+ Összeadás \n- Kivonás\n* Szorzás\n/ Osztás\n^ Hatványozás\n%% Maradékos osztás\n>",
-            .de = L"Es muss ein Operand angegeben werden! Diese können sein:\n+ Addition\n- Subtraktion\n* Multiplikation\n/ Division\n^ Potenzierung\n%% Modulo-Division\n>",
-            .en = L"You must provide an operand! These can be:\n+ Addition\n- Subtraction\n* Multiplication\n/ Division\n^ Raising to power\n%% Modulo-Division\n>"},
-
-        {.hu = L"A nullával való osztás nem megengedett!\n>",
-            .de = L"Division durch Null ist nicht erlaubt!\n>",
-            .en = L"Division by zero is not allowed!\n>"},
-
-        {.hu = L"A maradékos osztás csak egész számokkal megengedett\n>",
-            .de = L"Modulo-Division ist nur mit Ganzzahlen erlaubt\n>",
-            .en = L"Modulo-Division is only allowed with integers\n>"},
-    };
-
-    wchar_t welcomeString[146];
-    wchar_t resultString[146];
-    wchar_t missingOperandString[146];
-    wchar_t zeroDivisionString[146];
-    wchar_t moduloErrorString[146];
-
-    switch (lang) {
-        case 1:
-            wcscpy(welcomeString, translations[0].hu);
-            wcscpy(resultString, translations[1].hu);
-            wcscpy(missingOperandString, translations[2].hu);
-            wcscpy(zeroDivisionString, translations[3].hu);
-            wcscpy(moduloErrorString, translations[4].hu);
-            break;
-        case 2:
-            wcscpy(welcomeString, translations[0].de);
-            wcscpy(resultString, translations[1].de);
-            wcscpy(missingOperandString, translations[2].de);
-            wcscpy(zeroDivisionString, translations[3].de);
-            wcscpy(moduloErrorString, translations[4].de);
-            break;
-        case 3:
-            wcscpy(welcomeString, translations[0].en);
-            wcscpy(resultString, translations[1].en);
-            wcscpy(missingOperandString, translations[2].en);
-            wcscpy(zeroDivisionString, translations[3].en);
-            wcscpy(moduloErrorString, translations[4].en);
-            break;
-        default:
-            exit(1);
-    }
-
-    char operation[99];
-
-    wprintf(L"%ls", welcomeString);
-
-    wprintf(L"\n> "); fgets(operation, 99, stdin);
-
-
-    bool run = true;
-    while (run) {
-        const calculation CalcObject = evaluateOperation(operation);
-        if (CalcObject.operand == MissingOperand) {
-            wprintf(L"%ls ", missingOperandString);
-            fgets(operation, 99, stdin);
-            continue;
-        }
-
-        const result CalcResult = doCalculation(CalcObject);
-        if (CalcResult.errorCode == ZeroDivision) {
-            wprintf(L"%ls ", zeroDivisionString);
-            fgets(operation, 99, stdin);
-            continue;
-        }
-
-        if (CalcResult.errorCode == ModuloError) {
-            wprintf(L"%ls ", moduloErrorString);
-            fgets(operation, 99, stdin);
-            continue;
-        }
-
-        if (CalcResult.errorCode == Miscellaneous)
-            return 1;
-
-        wprintf(L"%ls " " %g\n\n", resultString, CalcResult.result);
-        run = false;
-    }
-
-    return 0;
 }
 
 calculation evaluateOperation(char *operation) {
@@ -256,4 +172,103 @@ result doCalculation(const calculation calculation) {
             result.errorCode = Miscellaneous;
             return result;
     }
+}
+
+void checkForErrors(StringArray stringArray) {
+
+    char operation[99];
+
+    wprintf(L"%ls", stringArray.welcome);
+
+    wprintf(L"\n> "); fgets(operation, 99, stdin);
+    bool run = true;
+    while (run) {
+        const calculation CalcObject = evaluateOperation(operation);
+        if (CalcObject.operand == MissingOperand) {
+            wprintf(L"%ls ", stringArray.missingOperand);
+            fgets(operation, 99, stdin);
+            continue;
+        }
+
+        const result CalcResult = doCalculation(CalcObject);
+        if (CalcResult.errorCode == ZeroDivision) {
+            wprintf(L"%ls ", stringArray.zeroDivision);
+            fgets(operation, 99, stdin);
+            continue;
+        }
+
+        if (CalcResult.errorCode == ModuloError) {
+            wprintf(L"%ls ", stringArray.moduloError);
+            fgets(operation, 99, stdin);
+            continue;
+        }
+
+        if (CalcResult.errorCode == Miscellaneous)
+            exit(1);
+
+        wprintf(L"%ls " " %g\n\n", stringArray.result, CalcResult.result);
+        run = false;
+    }
+}
+
+StringArray* setupLanguage(void) {
+    static StringArray chosenLanguage;
+
+    const struct { const wchar_t *hu, *de, *en; } translations[] = {
+        {.hu = L"A magyar nyelvet választottad!\n\nSzámológép\n",
+            .de = L"Sie haben die deutsche Sprache gewählt!\n\nTaschenrechner\n",
+            .en = L"You chose English!\n\nCalculator\n"},
+
+
+        {.hu = L"Ennek a számolásnak az eredménye:",
+            .de = L"Das Ergebnis dieser Berechnung:",
+            .en = L"The result of this calculation:"},
+
+        {.hu = L"Kötelező megadni egy operandust! Ezek lehetnek:\n+ Összeadás \n- Kivonás\n* Szorzás\n/ Osztás\n^ Hatványozás\n%% Maradékos osztás\n>",
+            .de = L"Es muss ein Operand angegeben werden! Diese können sein:\n+ Addition\n- Subtraktion\n* Multiplikation\n/ Division\n^ Potenzierung\n%% Modulo-Division\n>",
+            .en = L"You must provide an operand! These can be:\n+ Addition\n- Subtraction\n* Multiplication\n/ Division\n^ Raising to power\n%% Modulo-Division\n>"},
+
+        {.hu = L"A nullával való osztás nem megengedett!\n>",
+            .de = L"Division durch Null ist nicht erlaubt!\n>",
+            .en = L"Division by zero is not allowed!\n>"},
+
+            {.hu = L"A maradékos osztás csak egész számokkal megengedett!\n>",
+            .de = L"Modulo-Division ist nur mit Ganzzahlen erlaubt!\n>",
+            .en = L"Modulo-Division is only allowed with integers!\n>"},
+    };
+
+    long lang = 0;
+    int valid = 0;
+
+    do {
+        wprintf(L"\n\nVálassz egy nyelvet/Wähle eine Sprache/Choose a language:\n1 Magyar\n2 Deutsch\n3 English\n> ");
+        char langChar[3];
+        fgets(langChar, sizeof(langChar), stdin);
+        lang = strtol(langChar, NULL, 10);
+
+        if (lang >= 1 && lang <= 3)
+            valid = 1;
+    } while (!valid);
+
+    if (lang == 1) {
+        chosenLanguage.welcome        = translations[0].hu;
+        chosenLanguage.result         = translations[1].hu;
+        chosenLanguage.missingOperand = translations[2].hu;
+        chosenLanguage.zeroDivision   = translations[3].hu;
+        chosenLanguage.moduloError    = translations[4].hu;
+    } else if (lang == 2) {
+        chosenLanguage.welcome        = translations[0].de;
+        chosenLanguage.result         = translations[1].de;
+        chosenLanguage.missingOperand = translations[2].de;
+        chosenLanguage.zeroDivision   = translations[3].de;
+        chosenLanguage.moduloError    = translations[4].de;
+    } else {
+        chosenLanguage.welcome        = translations[0].en;
+        chosenLanguage.result         = translations[1].en;
+        chosenLanguage.missingOperand = translations[2].en;
+        chosenLanguage.zeroDivision   = translations[3].en;
+        chosenLanguage.moduloError    = translations[4].en;
+    }
+
+    return &chosenLanguage;
 }
