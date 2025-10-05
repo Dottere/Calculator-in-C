f629db63136b9c9876dd800ebee45571e89ae685
Balogh Tamás <balogh.t.1333@gmail.com>
2025-10-05 16:29:59 +0200

    implemented multiple languages

--- main.c
+++ main.c
@@ -21,6 +21,12 @@ typedef struct {
     int errorCode;
 } result;
 
+typedef struct {
+    wchar_t hu[126];
+    wchar_t de[146];
+    wchar_t en[135];
+} translations;
+
 enum Operand {
     ADDITION = 1,
     SUBTRACTION,
@@ -48,34 +54,94 @@ result doCalculation(calculation calculation);
 /*Terminal calculator app*/
 int main(void) {
     setlocale(LC_ALL, "");
+    setlocale(LC_NUMERIC, "C");
 
+    wprintf(L"Válassz egy nyelvet/Wähle eine Sprache/Choose a language:\n1 Magyar\n2 Detusch\n3 English\n> ");
+
+    char langChar[3];
+    fgets(langChar, 3, stdin);
+    const long lang = strtol(langChar, NULL, 10);
+
+    const translations translations[] = {
+        {.hu = L"Számológép\n",
+            .de = L"Taschenrechner\n",
+            .en = L"Calculator\n"},
+
+
+        {.hu = L"Ennek a számolásnak az eredménye:",
+            .de = L"Das Ergebnis dieser Berechnung:",
+            .en = L"The result of this calculation:"},
+
+        {.hu = L"Kötelező megadni egy operandust! Ezek lehetnek:\n+ Összeadás \n- Kivonás\n* Szorzás\n/ Osztás\n^ Hatványozás\n%% Maradékos osztás\n>",
+            .de = L"Es muss ein Operand angegeben werden! Diese können sein:\n+ Addition\n- Subtraktion\n* Multiplikation\n/ Division\n^ Potenzierung\n%% Modulo-Division\n>",
+            .en = L"You must provide an operand! These can be:\n+ Addition\n- Subtraction\n* Multiplication\n/ Division\n^ Raising to power\n%% Modulo-Division\n>"},
+
+        {.hu = L"A nullával való osztás nem megengedett!\n>",
+            .de = L"Division durch Null ist nicht erlaubt!\n>",
+            .en = L"Division by zero is not allowed!\n>"},
+
+        {.hu = L"A maradékos osztás csak egész számokkal megengedett\n>",
+            .de = L"Modulo-Division ist nur mit Ganzzahlen erlaubt\n>",
+            .en = L"Modulo-Division is only allowed with integers\n>"},
+    };
+
+    wchar_t welcomeString[146];
+    wchar_t resultString[146];
+    wchar_t missingOperandString[146];
+    wchar_t zeroDivisionString[146];
+    wchar_t moduloErrorString[146];
+
+    switch (lang) {
+        case 1:
+            wcscpy(welcomeString, translations[0].hu);
+            wcscpy(resultString, translations[1].hu);
+            wcscpy(missingOperandString, translations[2].hu);
+            wcscpy(zeroDivisionString, translations[3].hu);
+            wcscpy(moduloErrorString, translations[4].hu);
+            break;
+        case 2:
+            wcscpy(welcomeString, translations[0].de);
+            wcscpy(resultString, translations[1].de);
+            wcscpy(missingOperandString, translations[2].de);
+            wcscpy(zeroDivisionString, translations[3].de);
+            wcscpy(moduloErrorString, translations[4].de);
+            break;
+        case 3:
+            wcscpy(welcomeString, translations[0].en);
+            wcscpy(resultString, translations[1].en);
+            wcscpy(missingOperandString, translations[2].en);
+            wcscpy(zeroDivisionString, translations[3].en);
+            wcscpy(moduloErrorString, translations[4].en);
+            break;
+        default:
+            exit(1);
+    }
 
     char operation[99];
 
-    wprintf(L"###Számológép###");
+    wprintf(L"%ls", welcomeString);
 
-    printf("\n>"); fgets(operation, 99, stdin);
+    wprintf(L"\n> "); fgets(operation, 99, stdin);
 
 
     bool run = true;
     while (run) {
         const calculation CalcObject = evaluateOperation(operation);
         if (CalcObject.operand == MissingOperand) {
-            wprintf(L"Kötelező megadni egy operandust! Ezek lehetnek: \n+ összeadás"
-                "\n- kivonás\n* szorzás\n/ osztás\n^ hatványozás\n%% maradékos osztás\n>");
+            wprintf(L"%ls ", missingOperandString);
             fgets(operation, 99, stdin);
             continue;
         }
 
         const result CalcResult = doCalculation(CalcObject);
         if (CalcResult.errorCode == ZeroDivision) {
-            wprintf(L"A nullával való osztás nem értelmezhető!\n>");
+            wprintf(L"%ls ", zeroDivisionString);
             fgets(operation, 99, stdin);
             continue;
         }
 
         if (CalcResult.errorCode == ModuloError) {
-            wprintf(L"A maradékos osztás csak egész számokkal működik!\n>");
+            wprintf(L"%ls ", moduloErrorString);
             fgets(operation, 99, stdin);
             continue;
         }
@@ -83,7 +149,7 @@ int main(void) {
         if (CalcResult.errorCode == Miscellaneous)
             return 1;
 
-        wprintf(L"Ennek a számolásnak az eredménye: %g", CalcResult.result);
+        wprintf(L"%ls " " %g\n\n", resultString, CalcResult.result);
         run = false;
     }
 
@@ -99,8 +165,8 @@ calculation evaluateOperation(char *operation) {
     const size_t lenWithSpace = strlen(operation);
     int cleanIndex = 0;
     for (int dirtyIndex = 0; dirtyIndex < lenWithSpace; dirtyIndex++) {
-        if (operation[dirtyIndex] == '.') {
-            operation[dirtyIndex] = ','; // convert dot to comma
+        if (operation[dirtyIndex] == ',') {
+            operation[dirtyIndex] = '.';
         }
         if (operation[dirtyIndex] != ' ') {
             operationWithoutWhitespace[cleanIndex++] = operation[dirtyIndex];
@@ -168,6 +234,7 @@ result doCalculation(const calculation calculation) {
                 result.errorCode = ZeroDivision;
                 return result;
             }
+            printf("%f %f", calculation.num1, calculation.num2);
             result.result = calculation.num1 / calculation.num2;
             return result;
         case 5:
